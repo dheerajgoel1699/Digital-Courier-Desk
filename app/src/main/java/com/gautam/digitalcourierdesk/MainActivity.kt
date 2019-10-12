ec3d8ead628a16d42698ab3234dea553ecb15e02
gautambitious <mvgt.gautam@gmail.com>
2019-10-12 22:35:28 +0530

    Main Activity

--- app/src/main/java/com/gautam/digitalcourierdesk/MainActivity.kt
+++ app/src/main/java/com/gautam/digitalcourierdesk/MainActivity.kt
@@ -2,12 +2,24 @@ package com.gautam.digitalcourierdesk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        verifyOtp.setOnClickListener {
+
+//            startActivity<OtpCheckActivity>()
+        }
+
+
 
 
     }
