8741585697e2139324e5e265ba15cb8ac6c6a4e7
gautambitious <mvgt.gautam@gmail.com>
2019-10-18 01:55:46 +0530

    Otp Alert

--- app/src/main/java/com/gautam/digitalcourierdesk/OtpCheckActivity.kt
+++ app/src/main/java/com/gautam/digitalcourierdesk/OtpCheckActivity.kt
@@ -5,20 +5,11 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_otp_check.*
 
 class OtpCheckActivity : AppCompatActivity() {
-private var otp:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp_check)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        nameCheckLoad.hide()
-
-
-
-//        otp=Integer.parseInt(otpText.editText?.text.toString())
-
-
-
-
+//        nameCheckLoad.hide()
 
     }
 }
