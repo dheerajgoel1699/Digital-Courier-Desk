23fddda0a2bc378c56b42dd1713846199fc9b82a
gautambitious <mvgt.gautam@gmail.com>
2019-10-18 13:58:30 +0530

    update

--- app/src/main/java/com/gautam/digitalcourierdesk/OtpCheckActivity.kt
+++ app/src/main/java/com/gautam/digitalcourierdesk/OtpCheckActivity.kt
@@ -2,6 +2,7 @@ package com.gautam.digitalcourierdesk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_otp_check.*
 
 class OtpCheckActivity : AppCompatActivity() {
@@ -9,7 +10,7 @@ class OtpCheckActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp_check)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        nameCheckLoad.hide()
+        
 
     }
 }
