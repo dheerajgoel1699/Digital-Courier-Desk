6af0a2e11cab3e0e79d56fa42811c6deb62554c2
gautambitious <mvgt.gautam@gmail.com>
2019-11-06 20:47:46 +0530

    Update

--- app/src/main/java/com/gautam/digitalcourierdesk/MainActivity.kt
+++ app/src/main/java/com/gautam/digitalcourierdesk/MainActivity.kt
@@ -2,8 +2,13 @@ package com.gautam.digitalcourierdesk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.*
+
+val db by lazy{
+    FirebaseFirestore.getInstance()
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +23,16 @@ class MainActivity : AppCompatActivity() {
         }
         pastRecords.setOnClickListener {
             startActivity<PastAlertsActivity>()
+        }
+        resetSerial.setOnClickListener {
+            toast("Resting Serial Number")
+            resetSN()
+        }
+    }
+
+    private fun resetSN() {
+        db.collection("sn").document("sn").set(hashMapOf("sn" to 1)).addOnSuccessListener {
+            toast("Reset Successful")
         }
     }
 }
